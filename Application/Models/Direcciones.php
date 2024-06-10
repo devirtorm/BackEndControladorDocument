0c59c8a45a2385ed043bd5ebc17674ca202efd4b
Vicente de Jesus Meza Flores <vicentedejesusmezaflores2003@gmail.com>
2024-06-09 22:19:42 -0700

    Implementación y edición de funciones de la fecha y hora de los registros

--- Application/Models/Direcciones.php
+++ Application/Models/Direcciones.php
@@ -18,31 +18,35 @@ class ModelsDirecciones extends Model {
         return $data;
     }
     
-    public function DireccionesActivas() { //funciona
+    public function DireccionesActivas() {
         $sql = "SELECT * FROM direccion WHERE activo = 1";
         $query = $this->db->query($sql);
         $data = [];
-
+    
         if ($query->num_rows) {
             foreach ($query->rows as $value) {
+                $value['fecha'] = $this->formatDate($value['fecha']);
+                $value['hora'] = $this->formatTime($value['hora']);
                 $data[] = $value;
             }
         }
-
+    
         return $data;
     }
-
-    public function DireccionesInactivas() { //funciona
+    
+    public function DireccionesInactivas() {
         $sql = "SELECT * FROM direccion WHERE activo = 0";
         $query = $this->db->query($sql);
         $data = [];
-
+    
         if ($query->num_rows) {
             foreach ($query->rows as $value) {
+                $value['fecha'] = $this->formatDate($value['fecha']);
+                $value['hora'] = $this->formatTime($value['hora']);
                 $data[] = $value;
             }
         }
-
+    
         return $data;
     }
 
@@ -70,19 +74,24 @@ class ModelsDirecciones extends Model {
         }
     }    
 
-    public function createDireccion($data) { //funciona
+    public function createDireccion($data) {
+        // Establecer la zona horaria de Tepic, Nayarit, México
+        date_default_timezone_set('America/Mazatlan');
+        
+        // Obtener la fecha y hora actuales
         $nombre_direccion = $data['nombre_direccion'];
         $fecha = date('Y-m-d');
         $hora = date('H:i:s');
         $activo = 1;
-
+    
+        // Preparar y ejecutar la consulta SQL
         $sql = "INSERT INTO direccion (nombre_direccion, fecha, hora, activo) VALUES (?, ?, ?, ?)";
         $stmt = $this->db->prepare($sql);
         $stmt->bindParam(1, $nombre_direccion, PDO::PARAM_STR);
         $stmt->bindParam(2, $fecha, PDO::PARAM_STR);
         $stmt->bindParam(3, $hora, PDO::PARAM_STR);
         $stmt->bindParam(4, $activo, PDO::PARAM_INT);
-
+    
         return $stmt->execute();
     }
 
@@ -111,6 +120,16 @@ class ModelsDirecciones extends Model {
     
         // Ejecutar la eliminación y retornar el resultado
         return $stmt->execute();
+    }
+
+    private function formatDate($date) {
+        $dateTime = new DateTime($date);
+        return $dateTime->format('d-m-Y');
+    }
+    
+    private function formatTime($time) {
+        $dateTime = new DateTime($time);
+        return $dateTime->format('h:i:s A');
     }
 }
 
