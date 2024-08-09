6632acf7b6b3f1410d065d65520647ca9a368280
BlackieDeveloper <alexagame60@gmail.com>
2024-08-08 23:44:05 -0600

    datos aktualisados

--- config.php
+++ config.php
@@ -26,7 +26,7 @@ define('DATABASE', [
     'Driver' => 'PDO',
     'Name'   => 'portal_calidad4',
     'User'   => 'postgres',
-    'Pass'   => '11014423',
+    'Pass'   => 'Coah13',
     'Prefix' => 'l_'
 ]);
 
