dd89836b8b4c2a95d99d081badc3e06847c73a73
devirtorm <154764196+devirtorm@users.noreply.github.com>
2024-08-22 07:45:26 -0700

    para obtener cambios

--- config.php
+++ config.php
@@ -24,7 +24,7 @@ define('DATABASE', [
     'Port'   => '5432',
     'Host'   => 'localhost',
     'Driver' => 'PDO',
-    'Name'   => 'portal_calidad2',
+    'Name'   => 'portal_calidad',
     'User'   => 'postgres',
     'Pass'   => '0623',
     'Prefix' => 'l_'
