32c700a857ef0161dc65f6a8465460adb1a5b664
EliasBetanoz27 <eliiasbetanoz27@gmail.com>
2024-08-28 15:12:11 -0700

    validacion de documentos editados

--- config.php
+++ config.php
@@ -24,9 +24,9 @@ define('DATABASE', [
     'Port'   => '5432',
     'Host'   => 'localhost',
     'Driver' => 'PDO',
-    'Name'   => 'portal_calidad3',
+    'Name'   => 'portal_calidad9',
     'User'   => 'postgres',
-    'Pass'   => '0623',
+    'Pass'   => '12345',
     'Prefix' => 'l_'
 ]);
 
