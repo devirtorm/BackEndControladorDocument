da9094446f8b41d1be109c19b06ee395a37b77a7
devirtorm <154764196+devirtorm@users.noreply.github.com>
2024-11-10 17:39:22 -0700

    Copie archivo index antiguo y funcino

--- index.php
+++ index.php
@@ -5,20 +5,28 @@ require 'config.php';
 require SYSTEM . 'Startup.php';
 require 'vendor/autoload.php';
 
-
 // using
 use Router\Router;
 
 // create object of request and response class
 $request = new Http\Request();
 $response = new Http\Response();
-
-// pagination
 $pagination = new Pagination\Pagination();
 
-// set default header
+
+// Set CORS headers
 $response->setHeader('Access-Control-Allow-Origin: *');
-$response->setHeader("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+$response->setHeader('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+$response->setHeader('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+// Handle preflight requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    $response->sendStatus(200);
+    $response->render();
+    exit();
+}
+
+// set default header
 $response->setHeader('Content-Type: application/json; charset=UTF-8');
 
 // set request url and method
